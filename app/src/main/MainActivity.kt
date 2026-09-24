3d5f2e386e85b664ec2b7d71be74e2304a7ee8d3
ofgstudio3-devv <ofgstudio3@gmail.com>
2026-09-23 23:21:45 -0600

    Update MainActivity.kt

--- app/src/main/MainActivity.kt
+++ app/src/main/MainActivity.kt
@@ -7,10 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val textView = TextView(this)
-        textView.text = "Artemiss instalada!\nFunciona :D"
-        textView.textSize = 24f
-        textView.setPadding(50, 200, 50, 50)
-        setContentView(textView)
+        val tv = TextView(this)
+        tv.text = "Artemiss FUNCIONA!"
+        tv.textSize = 28f
+        tv.setPadding(40,300,40,40)
+        setContentView(tv)
     }
 }
